258d01979351619ad166e831c71646f758e816ad
Jeremy <jerahertz@gmail.com>
2018-05-17 17:58:33 -0400

    Fix menu for mobile and start work on venue section.

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,8 +1,11 @@
 function toggleMenu() {
     var navContainer = document.getElementById('nav-container');
-    navContainer.classList.toggle('hide');
-
+    var menuButton = document.getElementById('menu-button');
     var navLinks = document.getElementsByClassName('nav-link');
+    
+    navContainer.classList.toggle('hide');
+    menuButton.classList.toggle('hide');
+    
     Array.prototype.forEach.call(navLinks, element => {
         element.classList.toggle('hide');
     });
