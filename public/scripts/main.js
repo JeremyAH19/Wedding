26f0428e704e9e746b7b8d770fc50fb8355b80f5
Jeremy <jerahertz@gmail.com>
2018-05-29 17:50:24 -0400

    Add divider font. Change color scheme.

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,12 +1,36 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.body.addEventListener('click', event => {
+        let menuButton = document.getElementById('menu-button');
+        let isMobile = window.getComputedStyle(menuButton).getPropertyValue('display') !== 'none';
+
+        if (isMobile && event.target === '') {
+            hideMenu();
+        }
+    });
+});
+
 function toggleMenu() {
-    var navContainer = document.getElementById('nav-container');
-    var menuButton = document.getElementById('menu-button');
-    var navLinks = document.getElementsByClassName('nav-link');
+    let navContainer = document.getElementById('nav-container');
+    let menuButton = document.getElementById('menu-button');
+    let navLinks = document.getElementsByClassName('nav-link');
     
     navContainer.classList.toggle('hide');
     menuButton.classList.toggle('hide');
     
     Array.prototype.forEach.call(navLinks, element => {
         element.classList.toggle('hide');
+    });
+}
+
+function hideMenu() {
+    let navContainer = document.getElementById('nav-container');
+    let menuButton = document.getElementById('menu-button');
+    let navLinks = document.getElementsByClassName('nav-link');
+    
+    navContainer.classList.add('hide');
+    menuButton.classList.add('hide');
+    
+    Array.prototype.forEach.call(navLinks, element => {
+        element.classList.add('hide');
     });
 }
